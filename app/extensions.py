c5d2c4e24af94ec8cdde1df1d7eda10a7350ca13
NicolasRojas07 <149183682+NicolasRojas07@users.noreply.github.com>
2025-11-16 17:10:57 -0500

    Refactor MongoDB client initialization in extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,16 +1,15 @@
 import ssl
 from flask_pymongo import PyMongo
-from app.config import Config
 from pymongo import MongoClient
+from app.config import Config
+
+mongo = PyMongo()
 
 client = MongoClient(
     Config.MONGO_URI,
     tls=True,
-    tlsAllowInvalidCertificates=True,  
+    tlsAllowInvalidCertificates=True, 
     tlsVersion=ssl.PROTOCOL_TLSv1_2
 )
 
-from flask_pymongo import PyMongo
-
-mongo = PyMongo()
-mongo.cx = client 
+mongo.cx = client
