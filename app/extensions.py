aa0335ea9c5f79ae1d7c61a94cb05612b92358cd
NicolasRojas07 <149183682+NicolasRojas07@users.noreply.github.com>
2025-11-16 17:07:31 -0500

    Configure MongoDB client with TLS settings

--- app/extensions.py
+++ app/extensions.py
@@ -1,3 +1,16 @@
+import ssl
+from flask_pymongo import PyMongo
+from app.config import Config
+from pymongo import MongoClient
+
+client = MongoClient(
+    Config.MONGO_URI,
+    tls=True,
+    tlsAllowInvalidCertificates=True,  
+    tlsVersion=ssl.PROTOCOL_TLSv1_2
+)
+
 from flask_pymongo import PyMongo
 
 mongo = PyMongo()
+mongo.cx = client 
