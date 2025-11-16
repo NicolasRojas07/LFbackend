7d7b67a1525fe6f633ff051341c09a32b56e3f82
NicolasRojas07 <149183682+NicolasRojas07@users.noreply.github.com>
2025-11-16 17:05:26 -0500

    Change MongoDB URI and configure MongoClient
    Updated MongoDB URI for production use and added MongoClient configuration.

--- app/config.py
+++ app/config.py
@@ -1,7 +1,21 @@
 import os
+import pymongo
+from pymongo import MongoClient
+import ssl
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/mydb")
+    MONGO_URI = os.getenv(
+        "MONGO_URI",
+        "mongodb+srv://nicolas:2002@jwtcluster.fm231vs.mongodb.net/mydb"
+    )
     APP_SECRET = os.getenv("APP_SECRET", "dev_secret_change_this")
     DEBUG = os.getenv("DEBUG", "True").lower() in ("true", "1", "yes")
     ALLOWED_ALGORITHMS = ["HS256", "HS384", "HS512"]
+
+    client = MongoClient(
+        MONGO_URI,
+        tls=True,
+        tlsAllowInvalidCertificates=True,
+        tlsVersion=ssl.PROTOCOL_TLSv1_2
+    )
+    db = client.get_database()  
