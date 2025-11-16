95b04ac82dd2b8f89e967e87c8dfcbb2636d6430
NicolasRojas07 <149183682+NicolasRojas07@users.noreply.github.com>
2025-11-16 17:20:27 -0500

    Refactor MongoDB connection and remove PyMongo

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,6 @@
 import ssl
-from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from app.config import Config
-
-mongo = PyMongo()
 
 client = MongoClient(
     Config.MONGO_URI,
@@ -12,4 +9,4 @@ client = MongoClient(
     tlsVersion=ssl.PROTOCOL_TLSv1_2
 )
 
-mongo.cx = client
+db = client.get_database()
