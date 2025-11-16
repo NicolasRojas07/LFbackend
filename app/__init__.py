d207e4d7055e17b39b872eea9a43ba2a5b31c679
NicolasRojas07 <149183682+NicolasRojas07@users.noreply.github.com>
2025-11-16 17:18:52 -0500

    Remove MongoDB client setup from app initialization
    Removed MongoDB client initialization and related imports.

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,12 @@
-import os
 from flask import Flask
 from flask_cors import CORS
 import traceback
-import ssl
-from pymongo import MongoClient
 from app.config import Config
-from app.extensions import mongo
 from app.routes.jwt_routes import bp as jwt_bp
-
 
 def create_app(config_object=Config):
     app = Flask(__name__)
     app.config.from_object(config_object)
-
-    mongo_uri = os.environ.get("MONGO_URI", app.config.get("MONGO_URI"))
-    if not mongo_uri:
-        raise ValueError("MONGO_URI no está definido. Revisa las variables de entorno.")
-
-    client = MongoClient(
-        mongo_uri,
-        tls=True,
-        tlsAllowInvalidCertificates=True,
-        tlsVersion=ssl.PROTOCOL_TLSv1_2
-    )
-
-    mongo.init_app(app, mongo_client=client)
 
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
